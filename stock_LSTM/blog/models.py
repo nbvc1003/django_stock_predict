8bdd5b478f1c9c000b9fa0cc15dec42bfead250a
nbvc1003 <nbvc@nate.com>
2020-02-11 02:11:26 +0900

    나머지 작업

--- stock_LSTM/blog/models.py
+++ stock_LSTM/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+
+
 # Create your models here.
 
 class Post(models.Model):
@@ -13,6 +15,21 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- stock_LSTM/blog/views.py
+++ stock_LSTM/blog/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
-import os
-import matplotlib.pyplot as plt
-import numpy as np
-from keras import models, layers
-from sklearn.preprocessing import MinMaxScaler
-import pandas as pd
-import pandas_datareader as web
-from datetime import date, timedelta
 
 # Create your views here.
 def post_list(request):
@@ -22,6 +15,7 @@ def post_detail(request, pk):
 
     return render(request , 'blog/post_detail.html', {'post':post})
 
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -35,6 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html',{'form':form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -48,17 +43,30 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html',{'post':posts})
-
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
-
+@login_required
 def post_remove(request,pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
